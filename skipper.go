24bf0b92ded883d0c4640af95a68b5e307c76b9a
Sandor Szücs <sandor.szuecs@zalando.de>
2026-08-04 23:49:41 +0200

    feature: enable h2c for server handler, proxy client and net client
    ref: https://github.com/zalando/skipper/issues/4145
    ref: https://github.com/zalando/skipper/issues/1253
    
    Test Direct Prior Knowledge:
    ```
    % nghttp -v http://localhost:9001/path
    [  0.000] Connected
    [  0.000] send SETTINGS frame <length=12, flags=0x00, stream_id=0>
              (niv=2)
              [SETTINGS_MAX_CONCURRENT_STREAMS(0x03):100]
              [SETTINGS_INITIAL_WINDOW_SIZE(0x04):65535]
    [  0.001] send PRIORITY frame <length=5, flags=0x00, stream_id=3>
              (dep_stream_id=0, weight=201, exclusive=0)
    [  0.001] send PRIORITY frame <length=5, flags=0x00, stream_id=5>
              (dep_stream_id=0, weight=101, exclusive=0)
    [  0.001] send PRIORITY frame <length=5, flags=0x00, stream_id=7>
              (dep_stream_id=0, weight=1, exclusive=0)
    [  0.001] send PRIORITY frame <length=5, flags=0x00, stream_id=9>
              (dep_stream_id=7, weight=1, exclusive=0)
    [  0.001] send PRIORITY frame <length=5, flags=0x00, stream_id=11>
              (dep_stream_id=3, weight=1, exclusive=0)
    [  0.001] send HEADERS frame <length=43, flags=0x25, stream_id=13>
              ; END_STREAM | END_HEADERS | PRIORITY
              (padlen=0, dep_stream_id=11, weight=16, exclusive=0)
              ; Open new stream
              :method: GET
              :path: /path
              :scheme: http
              :authority: localhost:9001
              accept: */*
              accept-encoding: gzip, deflate
              user-agent: nghttp2/1.59.0
    [  0.001] recv SETTINGS frame <length=30, flags=0x00, stream_id=0>
              (niv=5)
              [SETTINGS_MAX_FRAME_SIZE(0x05):1048576]
              [SETTINGS_MAX_CONCURRENT_STREAMS(0x03):250]
              [SETTINGS_MAX_HEADER_LIST_SIZE(0x06):1048896]
              [SETTINGS_HEADER_TABLE_SIZE(0x01):4096]
              [SETTINGS_INITIAL_WINDOW_SIZE(0x04):1048576]
    [  0.001] send SETTINGS frame <length=0, flags=0x01, stream_id=0>
              ; ACK
              (niv=0)
    [  0.001] recv SETTINGS frame <length=0, flags=0x01, stream_id=0>
              ; ACK
              (niv=0)
    [  0.001] recv WINDOW_UPDATE frame <length=4, flags=0x00, stream_id=0>
              (window_size_increment=983041)
    [  0.008] recv (stream_id=13) :status: 201
    [  0.008] recv (stream_id=13) date: Tue, 04 Aug 2026 22:19:10 GMT
    [  0.008] recv (stream_id=13) server: Skipper
    [  0.008] recv HEADERS frame <length=36, flags=0x04, stream_id=13>
              ; END_HEADERS
              (padlen=0)
              ; First response header
    [  0.008] recv DATA frame <length=0, flags=0x01, stream_id=13>
              ; END_STREAM
    [  0.008] send GOAWAY frame <length=8, flags=0x00, stream_id=0>
              (last_stream_id=0, error_code=NO_ERROR(0x00), opaque_data(0)=[])
    ```
    
    test with upgrade does not work:
    ```
    % nghttp -uv http://localhost:9001/path
    [  0.000] Connected
    [  0.000] HTTP Upgrade request
    GET /path HTTP/1.1
    host: localhost:9001
    connection: Upgrade, HTTP2-Settings
    upgrade: h2c
    http2-settings: AAMAAABkAAQAAP__
    accept: */*
    user-agent: nghttp2/1.59.0
    
    [  0.002] HTTP Upgrade response
    HTTP/1.1 500 Internal Server Error
    Content-Length: 22
    Content-Type: text/plain; charset=utf-8
    Server: Skipper
    X-Content-Type-Options: nosniff
    Date: Tue, 04 Aug 2026 22:19:39 GMT
    
    Internal Server Error
    
    [ERROR] HTTP Upgrade failed
    Some requests were not processed. total=1, processed=0
    ```
    
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- skipper.go
+++ skipper.go
@@ -778,6 +778,14 @@ type Options struct {
 	// and the response messages during web socket upgrades.
 	ExperimentalUpgradeAudit bool
 
+	// EnableH2cServer enables h2c (HTTP/2 cleartext) on the incoming listener.
+	// No effect when TLS is configured (use TLS ALPN HTTP/2 instead).
+	EnableH2cServer bool
+
+	// EnableH2cBackends enables h2c (HTTP/2 cleartext) for outgoing backend connections.
+	// Backends must speak h2c.
+	EnableH2cBackends bool
+
 	// MaxLoopbacks defines the maximum number of loops that the proxy can execute when the routing table
 	// contains loop backends (<loopback>).
 	MaxLoopbacks int
@@ -1583,6 +1591,15 @@ func listenAndServeQuit(
 		IdleTimeout:       o.IdleTimeoutServer,
 		MaxHeaderBytes:    o.MaxHeaderBytes,
 		ErrorLog:          newServerErrorLog(),
+	}
+
+	if o.EnableH2cServer && !serveTLS {
+		if srv.Protocols == nil {
+			srv.Protocols = new(http.Protocols)
+		}
+		srv.Protocols.SetHTTP1(true)
+		srv.Protocols.SetUnencryptedHTTP2(true)
+		log.Info("h2c (HTTP/2 cleartext) enabled on listener")
 	}
 
 	cm := &skpnet.ConnManager{
@@ -2540,6 +2557,7 @@ func run(o Options, sig chan os.Signal, idleConnsCH chan struct{}) error {
 		ClientKeyFile:                    o.ClientKeyFile,
 		ClientCertRefreshInterval:        o.ClientCertRefreshInterval,
 		EnableMTLS:                       o.EnableMTLS,
+		EnableH2cBackends:                o.EnableH2cBackends,
 		CustomHttpRoundTripperWrap:       o.CustomHttpRoundTripperWrap,
 		RateLimiters:                     ratelimitRegistry,
 		EndpointRegistry:                 endpointRegistry,
